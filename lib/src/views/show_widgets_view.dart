533593f040eafae3416ef211d0e6edef460578b2
issel <isselcode@gmail.com>
2025-10-09 18:03:45 -0600

    style(ui): Crear (Radio Card y Choice Card Group)

--- lib/src/views/show_widgets_view.dart
+++ lib/src/views/show_widgets_view.dart
@@ -3,8 +3,25 @@ import 'package:arquitectura_cliente_sistema_vision/src/clean_features/widgets/c
 import 'package:arquitectura_cliente_sistema_vision/src/clean_features/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
-class ShowWidgetsView extends StatelessWidget {
+import '../clean_features/widgets/choice_card_group.dart';
+import '../clean_features/widgets/radio_card.dart';
+
+enum Role {
+  admin,
+  calidad,
+  tecnico
+}
+
+class ShowWidgetsView extends StatefulWidget {
   const ShowWidgetsView({super.key});
+
+  @override
+  State<ShowWidgetsView> createState() => _ShowWidgetsViewState();
+}
+
+class _ShowWidgetsViewState extends State<ShowWidgetsView> {
+
+  Role? role;
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +46,41 @@ class ShowWidgetsView extends StatelessWidget {
                 text: "Ingresar",
                 onTap: () => print("Ingresando"),
               ),
+
+              ChoiceCardGroup<Role>(
+                value: role,
+                onChanged: (v) => setState(() => role = v),
+                size: 100,
+                children: [
+                  RadioCard(
+                    value: Role.admin,
+                    groupValue: role,
+                    label: "Admin",
+                    asset: "assets/logo.png",
+                    onChanged: (p0) {
+                      print(p0);
+                    },
+                  ),
+                  RadioCard(
+                    value: Role.calidad,
+                    groupValue: role,
+                    label: "Calidad",
+                    asset: "assets/logo.png",
+                    onChanged: (p0) {
+                      print(p0);
+                    },
+                  ),
+                  RadioCard(
+                    value: Role.tecnico,
+                    groupValue: role,
+                    label: "Técnico",
+                    asset: "assets/logo.png",
+                    onChanged: (p0) {
+                      print(p0);
+                    },
+                  ),
+                ],
+              )
 
             ],
           ),

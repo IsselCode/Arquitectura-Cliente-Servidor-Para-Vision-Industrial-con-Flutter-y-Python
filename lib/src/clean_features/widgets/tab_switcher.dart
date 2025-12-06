1bd18d0dc2eaf00a5bb1ee798611db1ebedb61be
isselcode <isselcode@gmail.com>
2025-12-06 13:01:36 -0600

    fix(widget): Corregir el problema con el color de fondo del tabswitcher

--- lib/src/clean_features/widgets/tab_switcher.dart
+++ lib/src/clean_features/widgets/tab_switcher.dart
@@ -59,70 +59,72 @@ class _CustomWidgetState extends State<TabSwitcher> with SingleTickerProviderSta
     TextTheme textTheme = theme.textTheme;
     ColorScheme colorScheme = theme.colorScheme;
 
-    return InkWell(
-      borderRadius: BorderRadius.circular(10),
-      onTap: () => _toggleMovement(),
-      child: Ink(
-        height: widget.height,
-        width: double.infinity,
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        decoration: BoxDecoration(
-          color: widget.color ?? colorScheme.surface,
-          borderRadius: BorderRadius.circular(10)
-        ),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Stack(
-              children: [
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () => _toggleMovement(),
+        child: Ink(
+          height: widget.height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color:  widget.color ?? colorScheme.surface,
+          ),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Stack(
+                children: [
 
-                // Selected
-                AnimatedAlign(
-                  duration: Duration(milliseconds: 250),
-                  alignment: isLeftAlign() ? Alignment.centerLeft : Alignment.centerRight,
-                  child: Container(
-                    width: constraints.maxWidth / 2,
-                    decoration: BoxDecoration(
+                  // Selected
+                  AnimatedAlign(
+                    duration: Duration(milliseconds: 250),
+                    alignment: isLeftAlign() ? Alignment.centerLeft : Alignment.centerRight,
+                    child: Container(
+                      width: constraints.maxWidth / 2,
+                      decoration: BoxDecoration(
                         color: colorScheme.primary,
                         borderRadius: BorderRadius.circular(10)
-                    ),
+                      ),
 
+                    ),
                   ),
-                ),
 
-                // Button Texts
-                Row(
-                  children: [
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          widget.leftText,
-                          style: textTheme.bodyMedium?.copyWith(
-                            color: isLeftAlign() ? colorScheme.onPrimary : AppColors.grey,
-                            fontWeight: isLeftAlign() ? FontWeight.bold : FontWeight.normal
-                          ),
+                  // Button Texts
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            widget.leftText,
+                            style: textTheme.bodyMedium?.copyWith(
+                              color: isLeftAlign() ? colorScheme.onPrimary : AppColors.grey,
+                              fontWeight: isLeftAlign() ? FontWeight.bold : FontWeight.normal
+                            ),
+                          )
                         )
-                      )
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          widget.rightText,
-                          style: textTheme.bodyMedium?.copyWith(
-                            color: isLeftAlign() ? AppColors.grey : colorScheme.onPrimary,
-                            fontWeight: isLeftAlign() ? FontWeight.normal : FontWeight.bold,
-                          ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            widget.rightText,
+                            style: textTheme.bodyMedium?.copyWith(
+                              color: isLeftAlign() ? AppColors.grey : colorScheme.onPrimary,
+                              fontWeight: isLeftAlign() ? FontWeight.normal : FontWeight.bold,
+                            ),
+                          )
                         )
-                      )
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
 
 
-              ],
+                ],
 
 
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
